29c268caedf2f73850946d52baccb9c621066e0b
jsgjsg <110209803+jsgjsg@users.noreply.github.com>
2023-06-20 03:51:41 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("1\n0");
+        System.out.println(sb);
+    }
+}
