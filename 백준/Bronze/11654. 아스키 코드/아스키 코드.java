bb83f5bc99c3a4bd25b247c85ac28703535320d9
jsgjsg <110209803+jsgjsg@users.noreply.github.com>
2023-06-20 03:47:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int ASCII = br.readLine().charAt(0);
+        System.out.println(ASCII);
+    }
+}
