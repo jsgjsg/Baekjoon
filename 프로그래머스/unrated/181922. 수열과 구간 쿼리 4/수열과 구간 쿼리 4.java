12353ef10a8d650c0db20da8b081f93a9fbe9568
jsgjsg <110209803+jsgjsg@users.noreply.github.com>
2023-06-20 04:14:58 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 1.12 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[arr.length];
+        
+        for(int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            
+            for(int j = s; j <= e; j++) {
+                if(j % k == 0) answer[j]++;
+            }
+        }
+        for(int i = 0; i < arr.length; i++) answer[i] += arr[i];
+        
+        return answer;
+    }
+}
