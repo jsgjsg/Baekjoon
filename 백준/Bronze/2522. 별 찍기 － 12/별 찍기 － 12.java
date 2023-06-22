7f7c1b596733f03878db9db5b5f3d2de15bfd897
jsgjsg <110209803+jsgjsg@users.noreply.github.com>
2023-06-22 15:46:50 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        String star = "*";
+        for(int i = 1; i < N; i++) {
+            sb.append(" ".repeat(N - i)).append(star.repeat(i)).append("\n");
+        }
+        for(int i = N; i >= 1; i--) {
+            sb.append(" ".repeat(N - i)).append(star.repeat(i)).append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
