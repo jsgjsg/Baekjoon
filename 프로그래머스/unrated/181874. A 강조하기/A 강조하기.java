6d4e3a6a00a771f06e3eb93e99cd4fa5245712f0
jsgjsg <110209803+jsgjsg@users.noreply.github.com>
2023-08-17 20:28:22 +0900

    [unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,6 +1,16 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
+        String answer = myString.toLowerCase();
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < answer.length(); i++) {
+            char c = answer.charAt(i);
+            if(c == 'a') c = 'A';
+            
+            sb.append(c);
+        }
+        answer = sb.toString();
+        
         return answer;
     }
 }
