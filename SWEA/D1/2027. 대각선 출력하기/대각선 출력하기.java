d5f8daddbb4e22a5c2f2c606a5bfde175e1a04db
전승규 <110209803+jsgjsg@users.noreply.github.com>
2023-11-16 18:07:25 +0900

    [D1] Title: 대각선 출력하기, Time: 98 ms, Memory: 18,924 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("#++++\n+#+++\n++#++\n+++#+\n++++#");
+        System.out.println(sb);
+    }
+}
