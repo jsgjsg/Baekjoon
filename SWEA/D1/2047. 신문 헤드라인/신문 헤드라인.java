d1f2ce16ba8af8021dbbc8aedfe29694d2fca03d
전승규 <110209803+jsgjsg@users.noreply.github.com>
2023-11-16 17:35:07 +0900

    [D1] Title: 신문 헤드라인, Time: 100 ms, Memory: 18,732 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        s = s.toUpperCase();
+
+        System.out.println(s);
+    }
+}
