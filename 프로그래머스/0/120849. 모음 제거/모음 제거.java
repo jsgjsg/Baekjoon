a3fed674aee6f4485dfba749a1ba31d299a8d0e0
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-02-15 16:11:49 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') continue;
+            sb.append(c);
+        }
+        
+        answer = sb.toString();
+        
+        return answer;
+    }
+}
