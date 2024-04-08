d3c4ad61f0c51b0dbd2c7ca931d3cdb0d5e9bca4
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-04-08 13:47:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,21 +1,17 @@
 class Solution {
-    public long solution(int n) {
-        long answer = 0;
+    public int solution(int n) {
+        int answer = 2;
         
-        int n1 = 1;
-        int n2 = 2;
+        int pre = 1;
+        int tmp = 2;
         
         if(n == 1) return 1;
         if(n == 2) return 2;
         
         for(int i = 3; i <= n; i++) {
-            answer = (n1 + n2) % 1234567;
-            
-            if(i % 2 == 1) n1 += n2;
-            else n2 += n1;
-            
-            n1 %= 1234567;
-            n2 %= 1234567;
+            tmp = answer;
+            answer = (answer + pre) % 1234567;
+            pre = tmp;
         }
         
         return answer;
