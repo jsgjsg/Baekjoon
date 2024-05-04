74e0d953f14a8b651684ae79ec5729bd4bdd42d3
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-05-05 02:36:49 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,34 +1,9 @@
 function solution(str_list) {
     let answer = [];
     
-    let idxOfL = str_list.indexOf('l');
-    let idxOfR = str_list.indexOf('r');
-    
-    if(idxOfL + idxOfR == -2) return answer;
-    
-    if(idxOfL == -1) {
-        for(let i = idxOfR + 1; i < str_list.length; i++) {
-            answer.push(str_list[i]);
-        }
-        return answer;
-    }
-    if(idxOfR == -1) {
-        for(let i = 0; i < idxOfL; i++) {
-            answer.push(str_list[i]);
-        }
-        return answer;
-    }
-    if(idxOfL < idxOfR) {
-        for(let i = 0; i < idxOfL; i++) {
-            answer.push(str_list[i]);
-        }
-        return answer;
-    }
-    if(idxOfR < idxOfL) {
-        for(let i = idxOfR + 1; i < str_list.length; i++) {
-            answer.push(str_list[i]);
-        }
-        return answer;
+    for(let i = 0; i < str_list.length; i++) {
+        if(str_list[i] == 'l') return str_list.slice(0, i);
+        else if(str_list[i] == 'r') return str_list.slice(i + 1);
     }
     
     return answer;
