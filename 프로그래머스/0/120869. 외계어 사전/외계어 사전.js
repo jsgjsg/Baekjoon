bb68fd0862e634955b8bd01c28020b7b63086057
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-05-07 16:59:55 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -2,6 +2,7 @@ function solution(spell, dic) {
     let answer = 2;
     
     dic.forEach((item) => {
+        if(item.length != spell.length) return;
         let exist = true;
         spell.forEach((c) => {
             if(!item.includes(c)) exist = false;
