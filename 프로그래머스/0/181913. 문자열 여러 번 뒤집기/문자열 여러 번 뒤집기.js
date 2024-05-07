6b76e72ad4ed0697adbd962061661748f9b2895b
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-05-07 11:25:01 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 21.35 ms, Memory: 35.1 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -3,14 +3,8 @@ function solution(my_string, queries) {
     
     queries.forEach((item) => {
         let tmp = answer.slice(item[0], item[1] + 1).reverse();
-        let spl = answer.splice(item[0], item[1] - item[0] + 1);
-        
-        tmp.forEach((add, idx) => {
-            answer.splice(item[0] + idx, 0, add);
-        });
+        answer.splice(item[0], item[1] - item[0] + 1, ...tmp);
     });
     
-    answer = answer.join('');
-    
-    return answer;
+    return answer.join('');
 }
