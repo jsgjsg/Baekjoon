34713d535c53e55b452bb9e5faffb3e19a0b140c
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-05-08 08:57:14 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,14 @@
+function solution(d, budget) {
+    let answer = 0;
+    
+    d.sort((a, b) => a - b);
+    for(let i = 0; i < d.length; i++) {
+        if(budget - d[i] >= 0) {
+            budget -= d[i];
+            answer++;
+        }
+        else break;
+    }
+    
+    return answer;
+}
