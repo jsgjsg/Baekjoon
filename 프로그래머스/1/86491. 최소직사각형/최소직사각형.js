4bf774160433d769c85a92f032dc036b4fe23799
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-05-24 08:40:33 +0900

    [level 1] Title: 최소직사각형, Time: 21.32 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,24 +1,12 @@
 function solution(sizes) {
-    let answer = 0;
-    
-    let w = 1;
-    let h = 1;
+    let w = 1, h = 1;
     
     sizes.forEach((item) => {
-        item.sort();
-        // 그대로
-        let a = Math.max(w, item[0]) * Math.max(h, item[1]);
-        // 바꿔서
-        let b = Math.max(h, item[0]) * Math.max(w, item[1]);
+        item.sort((a, b) => a - b);
         
-        if(a < b) {
-            w = Math.max(w, item[0]);
-            h = Math.max(h, item[1]);
-        } else {
-            w = Math.max(w, item[1]);
-            h = Math.max(h, item[0]);
-        }
+        w = Math.max(item[0], w);
+        h = Math.max(item[1], h);
     })
     
-    return answer = h * w;
+    return w * h;
 }
