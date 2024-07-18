d1327ec2d396dd9f6d1c48f35d85bfb41a5f3443
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-07-19 08:54:40 +0900

    [level 1] Title: 카드 뭉치, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int idx1 = 0, idx2 = 0;
+        
+        for(int i = 0; i < goal.length; i++) {
+            String cur = goal[i];
+            if(idx1 < cards1.length && cur.equals(cards1[idx1])) idx1++;
+            else if(idx2 < cards2.length && cur.equals(cards2[idx2])) idx2++;
+            else return "No";
+        }
+        
+        return "Yes";
+    }
+}
